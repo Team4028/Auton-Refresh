3e9573fd7e2d63ac0c734be7b8474b84150d6cd5
WRueter <43909364+WRueter@users.noreply.github.com>
2020-07-20 23:36:43 -0400

    added util for speedscale

---
+++ src/main/java/com/swervedrivespecialties/exampleswerve/util/util.java
@@ -0,0 +1,9 @@
+package com.swervedrivespecialties.exampleswerve.util;
+
+public class util {
+
+    public static double getSpeedScale( double minSS, double additionalSS){
+        return minSS + (1 - minSS) * Math.pow(additionalSS, 2);
+    }
+
+}

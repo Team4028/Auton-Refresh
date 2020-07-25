cdb4d34d1c670381e98d8c16ef8b64f4d592561d
Wheeze2022 <46767973+Wheeze2022@users.noreply.github.com>
2020-07-25 12:08:20 -0400

    Mdales (#3)
    * RoboMap adds for BeakXboxController Class
    
    * Finishing Up util class for BeakXboxController

--- src/main/java/com/swervedrivespecialties/exampleswerve/RobotMap.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/RobotMap.java
@@ -20,4 +20,16 @@ public class RobotMap {
     public static final int PRIMARY_CONTROLLER_USB_PORT = 0; 
     public static final int SECONDARY_CONTROLLER_USB_PORT = 1;
     public static final int TERTIARY_CONTROLLER_USB_PORT = 2;
+
+    public static final int BUTTON_A = 1;
+    public static final int BUTTON_B = 2;
+    public static final int BUTTON_X = 3;
+    public static final int BUTTON_Y = 4;
+    public static final int BUTTON_LB = 5;
+    public static final int BUTTON_RB = 6;
+    public static final int BUTTON_BACK = 7;
+    public static final int BUTTON_START = 8;
+    public static final boolean LEFT_TRIGGER = true;
+    public static final boolean RIGHT_TRIGGER = false;
+    
 }

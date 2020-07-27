f160fd8934be274bcd1399050b1a6330df76f2b2
CadeReinberger <32687832+CadeReinberger@users.noreply.github.com>
2020-07-26 22:58:35 -0400

    Revert "Mdales (#3)"
    This reverts commit cdb4d34d1c670381e98d8c16ef8b64f4d592561d.

--- src/main/java/com/swervedrivespecialties/exampleswerve/util/BeakXboxController.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/util/BeakXboxController.java
@@ -7,9 +7,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class BeakXboxController{
 
-    public static final double DEADBAND = .05;
-
-    public XboxController controller;
+    private XboxController controller;
 
     public JoystickButton a;
     public JoystickButton b;
@@ -36,12 +34,9 @@ public class BeakXboxController{
         RIGHT_BUMPER = new JoystickButton(controller,RobotMap.BUTTON_RB);
         BACK = new JoystickButton(controller,RobotMap.BUTTON_BACK);
         START = new JoystickButton(controller,RobotMap.BUTTON_START);
-        LEFT_TRIGGER = new TriggerButton(controller,RobotMap.LEFT_TRIGGER); //true = left
-        RIGHT_TRIGGER = new TriggerButton(controller,RobotMap.RIGHT_TRIGGER); //false = right
-        DPAD_UP = new DpadButton(controller,Directions.UP);
-        DPAD_DOWN = new DpadButton(controller,Directions.DOWN);
-        DPAD_LEFT = new DpadButton(controller,Directions.LEFT);
-        DPAD_RIGHT = new DpadButton(controller,Directions.RIGHT);
+        LEFT_TRIGGER = new JoystickButton(controller,RobotMap.LEFT_TRIGGER); //true
+        RIGHT_TRIGGER = new JoystickButton(controller,RobotMap.RIGHT_TRIGGER); //false
+    //still need to create dpad directions
     }
     public double getLeftXAxis(){
         return controller.getRawAxis(0);
@@ -60,59 +55,6 @@ public class BeakXboxController{
     }
     public double getRightYAxis(){
         return controller.getRawAxis(5);
-    }
-     public static class TriggerButton extends Button {
-       private XboxController TB_Controller;
-       private boolean left;
-       private boolean right;
-       
-        public TriggerButton(XboxController controller, boolean left, boolean right){
-            TB_Controller = controller;
-           
-            if (controller.getLeftTrigger() > DEADBAND){
-                left = true;
-            } else {
-                left = false;
-            }
-            if (controller.getRightTrigger() > DEADBAND){
-                right = true;
-            } else {
-                right = false;
-            }
-        }
-        @Override
-        public boolean get(){
-            return TB_Controller.getTriggerAxis(left) > DEADBAND; 
-            return TB_Controller.getTriggerAxis(right) > DEADBAND;
-        }
-    }
-    public static class DpadButton extends Button{
-        public enum Directions {
-            UP(0),
-            DOWN(180),
-            RIGHT(90),
-            LEFT(270);
-
-            private final int angle;
-        
-            Direction(int angleVal){
-                angle = angleVal;
-            }
-            public int getAngle(){
-                return angleVal;
-            }
-        }
-        XboxController DPAD_CONTROLLER;
-        Directions direction;
-
-        public DpadButton(XboxController controller, Directions dir){
-            DPAD_CONTROLLER = controller;
-            direction = dir;
-        }
-        @Override
-        public boolean get(){
-            return DPAD_CONTROLLER.getPOV() == direction.getAngle();
-        }
     }
 }
 
